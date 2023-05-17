fc07804bc0907eb4a17c5d37eb06c2227944aade
barco71 <danielhf62@gmail.com>
2023-05-16 21:55:01 -0300

    corregido el formulario

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,5 @@
 function validarFormulario(){
+  const form = document.getElementById("formulario");
   const nombreInput = document.getElementsByName("Nombre")[0].value;
   const apellidoInput = document.getElementsByName("Apellido")[0].value;
   const correoInput = document.getElementsByName("Correo")[0].value;
@@ -11,93 +12,39 @@ function validarFormulario(){
   mensajeExito.innerHTML = "";
 
   //validar nombre
-  const expresionRegularLetras = /^[A-Za-z]+$/;
-  if (!expresionRegularLetras.test(nombreInput)) {
-    mensajeError.innerHTML = "Por favor, ingresa un nombre válido que contenga solo letras.";
-    return;}
-  if (nombreInput.length < 2){
-    mensajeError.innerHTML = "Por favor, ingresa un nombre valido con al menos dos caracteres.";
-    return
-  }
-  //validar apellido
-  if (!expresionRegularLetras.test(apellidoInput)) {
-    mensajeError.innerHTML = "Por favor, ingresa un apellido válido que contenga solo letras.";
-    return;}
-  if (apellidoInput.length < 2){
-    mensajeError.innerHTML = "Por favor, ingresa un apellido valido con al menos dos caracteres.";
-    return
-  }
-  //validar correo
-  const expresionRegularCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!expresionRegularCorreo.test(correoInput)) {
-    mensajeError.innerHTML = "Por favor, ingresa un correo electrónico válido.";
-    return;
-  }
-  
-  //validar telefono
-  const expresionRegularTelefono = /^\d{10}$/;
-  console.log(expresionRegularTelefono.test(telInput))
-  if (!expresionRegularTelefono.test(telInput)) {
-    mensajeError.textContent = "Por favor, ingresa un número de teléfono válido de 10 dígitos. Sin el 0 adelante";
-    return;
-  }
-  /*if (telInput.length < 10){
-    mensajeError.innerHTML = "Por favor, ingresa un número de teléfono válido de 10 dígitos."
-  }*/
-}
+    const expresionRegularLetras = /^[A-Za-z]+$/;
+    if (!expresionRegularLetras.test(nombreInput)) {
+      mensajeError.innerHTML = "Por favor, ingresa un nombre válido que contenga solo letras.";
+      return;}
+    if (nombreInput.length < 2){
+      mensajeError.innerHTML = "Por favor, ingresa un nombre valido con al menos dos caracteres.";
+      return
+    }
+    //validar apellido
+    if (!expresionRegularLetras.test(apellidoInput)) {
+      mensajeError.innerHTML = "Por favor, ingresa un apellido válido que contenga solo letras.";
+      return;}
+    if (apellidoInput.length < 2){
+      mensajeError.innerHTML = "Por favor, ingresa un apellido valido con al menos dos caracteres.";
+      return
+    }
+    //validar correo
+    const expresionRegularCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!expresionRegularCorreo.test(correoInput)) {
+      mensajeError.innerHTML = "Por favor, ingresa un correo electrónico válido.";
+      return;
+    }
+    
+    //validar telefono
+    const expresionRegularTelefono = /^\d{10}$/;
+    console.log(expresionRegularTelefono.test(telInput))
+    if (!expresionRegularTelefono.test(telInput)) {
+      mensajeError.textContent = "Por favor, ingresa un número de teléfono válido de 10 dígitos. Sin el 0 adelante";
+      return;
+    }
+    mensajeExito.innerHTML = "El formulario se envio correctamente.";
+    //document.getElementById("formulario").submit();
+    form.reset();
 
-/*function validarFormulario() {
-  const nombreInput = document.getElementsByName('Nombre')[0];
-  const apellidoInput = document.getElementsByName('Apellido')[0];
-  const correo = document.getElementsByName('correo')[0].value;
-  const telefono = document.getElementsByName('telefono')[0].value;
-  const mensaje = document.getElementById('mensaje').value;
-  const mensajeError = document.getElementById('mensajeError');
-  const mensajeExito = document.getElementById('mensajeExito');
-
-  // Reiniciar los mensajes
-  mensajeError.textContent = "";
-  mensajeExito.textContent = "";
-
-  // Validar nombre y apellido con expresión regular
-  const expresionRegularLetras = /^[A-Za-z]+$/;
-  if (!expresionRegularLetras.test(nombreInput.value)) {
-    mensajeError.textContent = "Por favor, ingresa un nombre válido que contenga solo letras.";
-    return;
-  }
-  if (nombreInput.value.length < 2) {
-    mensajeError.textContent = "Por favor, ingresa un nombre válido con al menos dos caracteres.";
-    return;
   }
 
-  if (!expresionRegularLetras.test(apellidoInput.value)) {
-    mensajeError.textContent = "Por favor, ingresa un apellido válido que contenga solo letras.";
-    return;
-  }
-  if (apellidoInput.value.length < 2) {
-    mensajeError.textContent = "Por favor, ingresa un apellido válido con al menos dos caracteres.";
-    return;
-  }
-
-  // Validar correo electrónico utilizando una expresión regular
-  const expresionRegularCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!expresionRegularCorreo.test(correo)) {
-    mensajeError.textContent = "Por favor, ingresa un correo electrónico válido.";
-    return;
-  }
-
-  // Validar número de teléfono utilizando una expresión regular
-  const expresionRegularTelefono = /^\d{10}$/;
-  if (!expresionRegularTelefono.test(telefono)) {
-    mensajeError.textContent = "Por favor, ingresa un número de teléfono válido de 10 dígitos.";
-    return;
-  }
-
-  if (mensaje === "") {
-    mensajeError.textContent = "Por favor, ingresa un mensaje.";
-    return;
-  }
-
-  //
-}
-*/
